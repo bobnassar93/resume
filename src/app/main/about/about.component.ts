e3b149fb46034be369edd7b2edec6b122370fe88
Bob Nassar <nassarbob@outlook.com>
2021-09-10 09:08:16 +0300

    Minor changes

--- src/app/main/about/about.component.ts
+++ src/app/main/about/about.component.ts
@@ -1,18 +1,38 @@
-import { Component, OnInit } from '@angular/core';
+import { AfterViewInit, Component, OnInit } from '@angular/core';
+import * as bootstrap from 'bootstrap';
 
 @Component({
   selector: 'app-about',
   templateUrl: './about.component.html',
   styleUrls: ['./about.component.scss']
 })
-export class AboutComponent implements OnInit {
+export class AboutComponent implements OnInit, AfterViewInit {
 
   age: number = 28;
+  bdMonth: boolean = false;
   constructor() { }
 
   ngOnInit(): void {
     const date = new Date();
     this.age = date.getFullYear() - 1993;
+
+    // if Month equals August
+    if (date.getMonth() === 7) {
+      this.bdMonth = true;
+    }
+  }
+
+  ngAfterViewInit(): void {
+    if (this.bdMonth === true)
+      this.enableToolTip();
+  }
+
+  enableToolTip(): void {
+    var span = document.querySelector('#bd') as HTMLSpanElement;
+    new bootstrap.Tooltip(span, {
+      title: 'Happy Birthday!',
+      placement: 'top'
+    })
   }
 
 }
